27b7b27a41f29564c909c2040d20b37af7c78487
rlukashenko <geroinya92@gmail.com>
2023-04-23 14:10:45 +0700

    time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [ time, setTime ] = useState(new Date())
+
+    useEffect(() => {
+        const timer = setInterval(() => setTime(new Date()), 1000)
+        return (() => {
+            clearInterval(timer)
+        })
+    })
+
   return (
     <div className="App">
       <header className="App-header">
 
         <p>
-          Hello world!
+            {time.toLocaleTimeString()}
         </p>
 
       </header>
